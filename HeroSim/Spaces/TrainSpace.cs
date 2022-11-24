07d42f2100c8f16897968e2ddefc72951fa9513e
Ryan Milem <gint271@gmail.com>
2022-11-23 17:20:37 -0800

    Adds bonus power to training spaces streaks.

--- HeroSim/Spaces/TrainSpace.cs
+++ HeroSim/Spaces/TrainSpace.cs
@@ -9,7 +9,15 @@ namespace HeroSim.Spaces
         public override void Land(BoardState state)
         {
             //TODO: Consecutive bonus.
-            state.power += 5;
+            switch (state.consecutiveTraining)
+            {
+                case 0: state.power += 5;
+                    break;
+                case 1: state.power += 10;
+                    break;
+                default: state.power += 20; // Assumes power gain stays at 20 for streaks above 3.
+                    break;
+            }
             state.consecutiveTraining += 1;
         }
     }
